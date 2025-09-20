f7bdf49ade938ec31b6f9ca87a7f600535be8e1f
Eleazar <programador3693@gmail.com>
2025-09-20 15:55:59 -0600

    Estructura inicial, siguiendo modelo de capas

---
+++ src/main/java/org/example/presentacion/Main.java
@@ -0,0 +1,14 @@
+package org.example.presentacion;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int nodos;
+
+        System.out.print("Ingresa la cantidad de nodos: ");
+        nodos = Integer.parseInt(entrada.nextLine());
+
+    }
+}
